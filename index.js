4e062eb605b6ecc27c49d3d97168ff46d265d5e1
itsk4to <hoaant9@gmail.com>
2026-09-20 12:06:51 +0700

    Enhance fetch method with path and token validation

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 export default {
-  async fetch(request) {
-    return new Response("K-1 is online");
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname !== "/k1") {
+      return new Response("Not Found", {
+        status: 404
+      });
+    }
+
+    const token = url.searchParams.get("token");
+
+    if (!token || token !== env.K1_TOKEN) {
+      return new Response("Unauthorized", {
+        status: 401
+      });
+    }
+
+    return new Response("K-1 access granted", {
+      status: 200
+    });
   }
 };
